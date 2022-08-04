7bee3883189dc6879303967f71f11848153283dc
norlog86 <vladsolyanka@gmail.com>
2022-08-04 08:40:37 +0300

    v.1.0.2 Создание CRUD для materials

---
+++ views/materials/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Materials */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="materials-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'blog_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
